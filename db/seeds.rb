5adbd339f7d863f61d804624512cf00311017b55
mrh9ud <mrh9ud@virginia.edu>
2019-11-21 16:46:31 -0500

    active status works

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.destroy_all
+matt = Student.create(first_name: "Matt", last_name: "Heavner", active: true)
+remi = Student.create(first_name: "Remi", last_name: "Odufuye")
