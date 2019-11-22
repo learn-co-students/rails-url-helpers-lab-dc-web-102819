3f2db9522dfb5043eeebb6392e55fbe9b40d7372
github username <github email address>
2019-11-22 12:45:03 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,39 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :edit]
+
   def index
     @students = Student.all
   end
 
-  def show
+  def new
+
+  end
+  def create
+
   end
 
-  private
+  def activate
+    set_student
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
 
+  def show
+  end
+  def edit
+  end
+  def update
+    # @student.active = true
+    # @student.save
+  end
+
+  def destroy
+
+  end
+
+
+  private
     def set_student
       @student = Student.find(params[:id])
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :students, only: [:index, :show]
+  get '/', to: "students#index"
+  resources :students
+  #Research/Ask for clarification of this code: GET TO AS
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
+
 end
